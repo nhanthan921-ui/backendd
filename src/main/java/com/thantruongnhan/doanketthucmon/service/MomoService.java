8ba4ad191b9c8bb25127264327229e1651310e1a
NhanThan271 <nhantran.13082005@gmail.com>
2025-12-24 15:53:58 +0700

    update

--- src/main/java/com/thantruongnhan/doanketthucmon/service/MomoService.java
+++
@@ -1,7 +0,0 @@
-package com.thantruongnhan.doanketthucmon.service;
-
-import java.util.Map;
-
-public interface MomoService {
-    Map<String, Object> createPayment(String orderId, Long amount, String orderInfo) throws Exception;
-}
