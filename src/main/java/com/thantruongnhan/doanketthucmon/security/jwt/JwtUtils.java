cc6cc33c127c5d2f5524983f0b3bd053fdd14341
NhanThan271 <nhantran.13082005@gmail.com>
2026-01-03 01:12:06 +0700

    update

--- src/main/java/com/thantruongnhan/doanketthucmon/security/jwt/JwtUtils.java
+++ src/main/java/com/thantruongnhan/doanketthucmon/security/jwt/JwtUtils.java
@@ -30,6 +30,9 @@ public class JwtUtils {
     private int jwtExpirationMs;
 
     public String generateJwtToken(Authentication authentication) {
+
+        System.out.println("JWT SECRET = " + jwtSecret);
+
         UserDetailsImpl userPrincipal = (UserDetailsImpl) authentication.getPrincipal();
 
         return Jwts.builder()
