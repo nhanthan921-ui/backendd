86c0cda09818ba9b3b367efc2c4f7e64b0d4efa3
NhanThan271 <nhantran.13082005@gmail.com>
2026-01-17 05:18:44 +0700

    Initial deployment

--- src/main/java/com/thantruongnhan/doanketthucmon/repository/MovieRepository.java
+++ src/main/java/com/thantruongnhan/doanketthucmon/repository/MovieRepository.java
@@ -19,11 +19,6 @@ public interface MovieRepository extends JpaRepository<Movie, Long> {
 
     List<Movie> findByGenresId(Long genreId);
 
-    @Query("SELECT DISTINCT m FROM Movie m JOIN m.genres g JOIN m.showtimes s WHERE g.id = :genreId AND s.cinema.id = :cinemaId AND m.status = :status")
-    List<Movie> findByGenresIdAndCinemaIdAndStatus(
-            @Param("genreId") Long genreId,
-            @Param("status") MovieStatus status);
-
     @Query("SELECT DISTINCT m FROM Movie m JOIN m.genres g WHERE g.id = :genreId AND m.status = :status")
     List<Movie> findByGenresIdAndStatus(@Param("genreId") Long genreId, @Param("status") MovieStatus status);
 }
